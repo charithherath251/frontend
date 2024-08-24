c31cb9e200f6315a4106cae0c0bfd6aa1c4ebea2
pehan08 <it22169662@my.sliit.lk>
2024-08-24 16:10:26 +0530

    Tailwind color setup

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,8 @@ import { Route, Router, Routes } from 'react-router-dom';
 import Sidebar from './components/SideBar';
 import Home from './pages/Home';
 import PolicyPage from './components/PoliciesPage';
+import Login from './pages/Login';
+
 
 function App() {
   return (
@@ -13,6 +15,7 @@ function App() {
           <Route exact path="/" element={<Sidebar/>} />
           <Route path='/home' element={<Home/>} /> 
           <Route path='/policies' element={<PolicyPage/>} />
+          <Route path='/login' element={<Login/>} />
         </Routes>
     </>
   );
---
+++ src/pages/Login.jsx
@@ -0,0 +1,51 @@
+import React from 'react';
+
+const Login = () => {
+    return (
+        <div className="flex items-center justify-center h-screen ">
+            <div className="bg-serenity shadow-md rounded px-8 pt-6 pb-8 mb-4">
+                <h2 className="text-2xl font-bold mb-6">Login</h2>
+                <form>
+                    <div className="mb-4">
+                        <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="username">
+                            Username
+                        </label>
+                        <input
+                            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                            id="username"
+                            type="text"
+                            placeholder="Enter your username"
+                        />
+                    </div>
+                    <div className="mb-6">
+                        <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="password">
+                            Password
+                        </label>
+                        <input
+                            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                            id="password"
+                            type="password"
+                            placeholder="Enter your password"
+                        />
+                    </div>
+                    <div className="flex items-center justify-between">
+                        <button
+                            className="bg-blue-900 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+                            type="button"
+                        >
+                            Sign In
+                        </button>
+                        <a
+                            className="inline-block align-baseline font-bold text-sm text-blue-500 hover:text-blue-800"
+                            href="#"
+                        >
+                            Forgot Password?
+                        </a>
+                    </div>
+                </form>
+            </div>
+        </div>
+    );
+};
+
+export default Login;
